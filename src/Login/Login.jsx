37ad63d919e8296256f01a8aaf3b74749f34980a
Prathammehra7 <manshumehra7@gmail.com>
2022-07-18 12:02:31 +0530

    update css

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from "../navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Loginpage from "./Loginpage/Loginpage"
+import "./loginpage.css"
+
 
 export default function Login() {
   return (
